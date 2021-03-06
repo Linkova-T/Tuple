5c42963eb769a810f4f2730647fb440780f43d5c
Linkova-T <78982154+Linkova-T@users.noreply.github.com>
2021-03-06 23:13:55 +0300

    Update Tuple.cpp
    new cout

--- Tuple/Tuple.cpp
+++ Tuple/Tuple.cpp
@@ -29,6 +29,6 @@ std::ostream& operator << (std::ostream& stream, const std::tuple < Types ... >&
 int main() {
 	auto tuple = std::make_tuple("Alice", 2, "r", 343434);
 	std::cout << tuple << std::endl;
-	std::cout << "The end]]]\";
+	std::cout << "I want to sleep";
 	return 0;
 }
